9117549d880cda51494d2312347dbcaa7973f63d
Patrick <dongyang0216@gmail.com>
2024-05-24 23:14:00 +0800

    fix modal and delete homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_cart, if: :user_signed_in?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname])
   end
 
+
+  def set_cart
+    if user_signed_in?
+      @cart = current_user.cart
+      @cart_items  = @cart&.cart_items || []
+      if @cart
+        @cart_items = @cart.cart_items.includes(:product) # Include product to avoid N+1 queries
+      else
+        @cart_items = []
+      end
+    end
+  end
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
--- app/controllers/customer_client/cart_controller.rb
+++ app/controllers/customer_client/cart_controller.rb
@@ -3,6 +3,12 @@ class CustomerClient::CartController < ApplicationController
 
   def index
     @cart = current_user.cart
+    @cart_items  = @cart&.cart_items || []
+    if @cart
+      @cart_items = @cart.cart_items.includes(:product) # Include product to avoid N+1 queries
+    else
+      @cart_items = []
+    end
   end
 
 end
