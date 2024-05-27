f9dfd3d5b2d47f11730ba2948544954e32181f49
jamillessn <jamillerosesison1@gmail.com>
2024-05-27 13:53:22 +0800

    style: remove header in login and registration, update price and product title design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    #For rendering the header when the user is in the login/registration page.
+    def devise_controller?
+        controller.class < DeviseController
+      end
+    
+    def devise_page?
+        devise_controller? && (action_name == 'new' || action_name == 'create')
+    end
 end
