0b416b8f64af1e0e1d57a80561f74afcd35407d1
Jeffrey Biñas <binasjeffrey34@yahoo.com>
2024-06-04 20:12:43 +0800

    Commit to push only.

---
+++ db/migrate/20240604120331_add_column_status_to_orders.rb
@@ -0,0 +1,6 @@
+class AddColumnStatusToOrders < ActiveRecord::Migration[7.1]
+  def change
+    add_column :orders, :status, :integer, default: 0
+    add_column :orders, :total, :decimal
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_03_071732) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_04_120331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_03_071732) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
@@ -117,9 +118,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_03_071732) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "payment_id"
     t.integer "status", default: 0
     t.decimal "total"
-    t.string "payment_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
