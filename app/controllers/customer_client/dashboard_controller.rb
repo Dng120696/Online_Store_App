3232cfd0b3dfc6c6f7b2dbdc162a1dd6e2356ffb
Patrick <dongyang0216@gmail.com>
2024-06-08 02:11:55 +0800

    added logic for getting product for new releases and best seller

--- app/controllers/customer_client/dashboard_controller.rb
+++ app/controllers/customer_client/dashboard_controller.rb
@@ -3,22 +3,30 @@ class CustomerClient::DashboardController < ApplicationController
   def index; end
 
   def load_products
-    @category = Category.find_by(title: params[:category].empty? ? 'Best Seller' : params[:category])
-
     search_product = params[:search]
+    params_category = params[:category]
 
-    if @category
-      @products = if search_product.present?
-                    @category.products.where("LOWER(name) LIKE ?", "%#{search_product.downcase}%")
-                                    .includes(image_attachment: :blob)
-                                    .order(order_items_count: :desc, inventory_level: :desc)
-                  else
-                    @category.products.includes(image_attachment: :blob)
-                                    .order(order_items_count: :desc, inventory_level: :desc)
-                  end
+    if params_category == 'Best Seller'
+      @products = Product.where("order_items_count > 10").includes(image_attachment: :blob).order(order_items_count: :desc)
+    elsif params_category == 'New Releases'
+      @products = Product.where("created_at >= ?", 12.hours.ago).includes(image_attachment: :blob)
     else
-      @products = []
+    @category = Category.find_by(title: params_category.empty? ? 'Best Seller' : params_category)
+
+      if @category
+        @products = if search_product.present?
+                      @category.products.where("LOWER(name) LIKE ?", "%#{search_product.downcase}%")
+                                      .includes(image_attachment: :blob)
+                                      .order(order_items_count: :desc, inventory_level: :desc)
+                    else
+                      @category.products.includes(image_attachment: :blob)
+                                      .order(order_items_count: :desc, inventory_level: :desc)
+                    end
+      else
+        @products = []
+      end
     end
+
 
     @cart = current_user&.cart
     @cart_items = @cart ? @cart.cart_items.includes(product: { image_attachment: :blob }) : []
