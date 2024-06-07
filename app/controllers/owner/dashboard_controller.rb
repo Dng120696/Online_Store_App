65ab09359f370a212df262c235050d598bab0c8e
Patrick <dongyang0216@gmail.com>
2024-06-08 02:48:53 +0800

    fix chart

--- app/controllers/owner/dashboard_controller.rb
+++ app/controllers/owner/dashboard_controller.rb
@@ -8,11 +8,15 @@ class Owner::DashboardController < ApplicationController
 
     @order_list = Order.where(status: @params_status)
 
-   # get the total ordersales every day
-   orders_data = Order.where(created_at: 1.month.ago.beginning_of_day..Time.zone.now.end_of_day).group_by_day(:created_at).sum(:total)
+    orders = Order.where(created_at: 1.month.ago.beginning_of_day..Time.zone.now.end_of_day)
+
+    # Group by day and sum the totals
+    orders_data = orders.group_by { |order| order.created_at.end_of_day }
+                                 .transform_values { |orders| orders.sum(&:total) }
 
    # Prepare data for Chartkick
    @orders_data = []
-   orders_data.each { |date, order_count| @orders_data << [ date, order_count ] }
+   orders_data.each { |date, order_count| @orders_data << [ date ,order_count ] }
+   p @orders_data
   end
 end
