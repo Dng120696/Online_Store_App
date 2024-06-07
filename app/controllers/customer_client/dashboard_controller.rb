ec99a897fca7246805684e1797a7081978b71d83
jamillessn <jamillerosesison1@gmail.com>
2024-06-07 18:47:20 +0800

    search bar fix

--- app/controllers/customer_client/dashboard_controller.rb
+++ app/controllers/customer_client/dashboard_controller.rb
@@ -3,7 +3,7 @@ class CustomerClient::DashboardController < ApplicationController
   def index; end
 
   def load_products
-    @category = Category.find_by(title: params[:category])
+    @category = Category.find_by(title: params[:category].empty? ? 'Shop All' : params[:category])
     search_product = params[:search]
 
     if @category
