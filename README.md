fe1b4fa777ced03340bbf4efbd5ba956b246c71d
Patrick <dongyang0216@gmail.com>
2024-06-07 20:12:05 +0800

    delete unnecesary

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Welcome to our Online Store App, a robust web application built with Ruby on Rai
 ## Table of Contents
 1. [Features](#features)
 2. [Images](#images)
-2. [Testing App](#testing-app)
 3. [Getting Started](#getting-started)
 4. [System Dependencies](#system-dependencies)
 5. [Contact Information](#contact-information)
@@ -108,11 +107,6 @@ Welcome to our Online Store App, a robust web application built with Ruby on Rai
 Check out the live demo of the app in the link below:
 
 [Online Store Application](https://online-store-app-development.onrender.com)
-
-## Testing App
-  ### Customer Account 
-    - Email: "user@user.com"
-    - Password: "password123"
 
 ## Getting Started
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
 
 Admin.create(firstname: 'Patrick',lastname: 'Nebab', email: 'admin@admin.com',password:'password123')
 admin = Admin.first
-user = User.new(firstname: 'Pat',lastname: 'Nebab', email: 'user@user.com',password:'password123',
+User.create(firstname: 'Pat',lastname: 'Nebab', email: 'user@user.com',password:'password123',
  address_attributes:  {
   street: '123 Tugue Street',
   city: 'Tuguegarao',
@@ -18,8 +18,7 @@ user = User.new(firstname: 'Pat',lastname: 'Nebab', email: 'user@user.com',passw
   country: 'Philippines',
   zip_code: '3519'
 })
-user.skip_confirmation!
-user.save
+
 admin.categories.create(title:"Best Seller")
 admin.categories.create(title:"New Releases")
 admin.categories.create(title:"Books")
